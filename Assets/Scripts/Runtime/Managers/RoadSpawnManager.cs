1ece622bbffb8074e745820e14126ef286a47926
deathatred <maksym.linynskyi@gmail.com>
2025-07-31 18:32:38 +0300

    Ver.1 trying adding pooling

--- Assets/Scripts/Runtime/Managers/RoadSpawnManager.cs
+++ Assets/Scripts/Runtime/Managers/RoadSpawnManager.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 public class RoadSpawner : MonoBehaviour
 {
     [SerializeField] private RoadSegment roadPrefab;
-    [SerializeField] private int initialPoolSize = 6;
+    [SerializeField] private int initialPoolSize = 7;
     [SerializeField] private float segmentLength = 10f;
-    [SerializeField] private float despawnZ = -15f;
+    [SerializeField] private float despawnZ = -25f;
 
     private ObjectPool<RoadSegment> pool;
     private List<RoadSegment> activeSegments = new List<RoadSegment>();
-    private float nextZ = 0f;
 
     void Start()
     {
@@ -42,13 +41,14 @@ public class RoadSpawner : MonoBehaviour
     void SpawnRoad()
     {
         RoadSegment newSegment = pool.Get();
-
+        float ZOffset = 0.001f;
         float spawnZ = activeSegments.Count > 0
-            ? activeSegments[activeSegments.Count - 1].transform.position.z + segmentLength
+            ? (activeSegments[activeSegments.Count - 1].transform.position.z+ ZOffset) + segmentLength
             : 0f;
 
         Vector3 spawnPos = new Vector3(0f, 0f, spawnZ);
         newSegment.Activate(spawnPos);
+        newSegment.GetComponent<RoadObstacleSpawn>().Spawn();
         activeSegments.Add(newSegment);
     }
 }
---
+++ Assets/Scripts/Runtime/Road/RoadObstacleSpawn.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class RoadObstacleSpawn : MonoBehaviour
+{
+    [SerializeField] private ObjectPool<Obstacle> _obstaclePool;
+    [SerializeField] private Obstacle _obstacleSpawnPrefab;
+    private int _poolSize = 20;
+    private float _chance = 0.1f;
+    private float _segmentLength = 20f;
+
+    private ObjectPool<Obstacle> pool;
+
+    private void Awake()
+    {
+        pool = new ObjectPool<Obstacle>(_obstacleSpawnPrefab,_poolSize);
+    }
+    public void Spawn()
+    {
+        if (Random.value > _chance)
+            return;
+
+        float randomX = 2 * Random.Range(-1, 2);
+        float randomZ = Random.Range(0f, _segmentLength);
+
+        Vector3 localOffset = new Vector3(randomX, 1f, randomZ);
+        Vector3 worldPos = transform.position + localOffset;
+
+        Obstacle obj = _obstaclePool.Get();
+        obj.transform.position = worldPos;
+        obj.transform.rotation = Quaternion.identity;
+        obj.transform.SetParent(transform);
+    }
+    public void SetPool(ObjectPool<Obstacle> pool)
+    {
+        _obstaclePool = pool;
+    }
+}
